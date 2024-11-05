a74c3a08fd519344d19dc763e5a4cca584f594e2
ASantiagoNunezG <abraham.nunez@tecsup.edu.pe>
2024-11-05 12:12:22 -0500

    Proyecto Inicializado

---
+++ AlquilaCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlquilaCar
+//
+//  Created by Mac13 on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
